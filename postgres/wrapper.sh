77eb0a18c5775b4ba702df3192dfed5f2e2dc665
Bryan Lee <me@bryanl.ee>
2026-08-27 20:45:48 -0700

    fix: only copy custom data on first start

--- postgres/wrapper.sh
+++ postgres/wrapper.sh
@@ -27,10 +27,10 @@ sed -i -e 's/data_directory = '\''\/var\/lib\/postgresql\/data'\''/data_director
 if [[ ! -e "/var/lib/postgresql/data/custom" ]]; then
   mkdir -p /var/lib/postgresql/data/custom
   chown postgres:postgres /var/lib/postgresql/data/custom
+  yes | cp -arf /etc/postgresql-custom/* /var/lib/postgresql/data/custom
 fi
 # If custom directory isnt "mounted", copy any changed configs and "mount" it
 if ! [[ -L "/etc/postgresql-custom" && -d "/var/lib/postgresql/data/custom" ]]; then
-  yes | cp -arf /etc/postgresql-custom/* /var/lib/postgresql/data/custom
   rm -rf /etc/postgresql-custom
   ln -s /var/lib/postgresql/data/custom /etc/postgresql-custom
 fi
